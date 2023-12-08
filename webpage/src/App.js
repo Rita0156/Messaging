09141a619bd8dfddea8d2fc179d5edbee61b0e92
Rita0156 <ritachaudhari006@gmail.com>
2023-12-08 16:04:58 +0530

    home

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Homepage from "./Pages/Homepage"
-import LoginFun from './Pages/Loginpage';
+
 import { AllRoutes } from './Pages/AllRoutes';
 //import LoginFun from
 //import {AllRoutes} from "./Pages/AllRoutes"
